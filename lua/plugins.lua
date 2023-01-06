35d7b87cdf44a555039862fa9fc90ee6e5b31009
Matthew Christensen <mchristensen@mq42n93wmg.lan>
2023-01-06 11:30:52 -0700

    added which-key

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -27,6 +27,18 @@ return require('packer').startup(function(use)
       }
     }
 
+  use {
+    "folke/which-key.nvim",
+    config = function()
+      require("which-key").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end
+  }
+
+
 	-- Post-install/update hook with neovim command
 	--use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
 	--
